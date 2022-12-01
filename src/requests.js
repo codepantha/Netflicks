59884b2bd1f8214d649cd5e775eab3b705e1c567
codepanther <pezechigozirim@gmail.com>
2022-12-01 12:25:17 +0100

    Fix error in requests url path

--- src/requests.js
+++ src/requests.js
@@ -5,10 +5,10 @@ const requests = {
   fetchNetflixOriginals: `/discover/tv?api_key=${apiKey}&with_networks=213`,
   fetchTopRated: `/movie/top_rated?api_key=${apiKey}&language=en-US`,
   fetchActionMovies: `/discover/movie?api_key=${apiKey}&with_genres=28`,
-  fetchComedyMovies: `/discover?movie?api_key=${apiKey}&with_genres=35`,
-  fetchHorrorMovies: `/discover?movie?api_key=${apiKey}&with_genres=27`,
-  fetchRomanceMovies: `/discover?movie?api_key=${apiKey}&with_genres=10749`,
-  fetchDocumentaries: `/discover?movie?api_key=${apiKey}&with_genres=99`
+  fetchComedyMovies: `/discover/movie?api_key=${apiKey}&with_genres=35`,
+  fetchHorrorMovies: `/discover/movie?api_key=${apiKey}&with_genres=27`,
+  fetchRomanceMovies: `/discover/movie?api_key=${apiKey}&with_genres=10749`,
+  fetchDocumentaries: `/discover/movie?api_key=${apiKey}&with_genres=99`
 };
 
 export default requests;
