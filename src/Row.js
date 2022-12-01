727cc510b1886d06e5fc29f538cd410d2dca57c9
codepanther <pezechigozirim@gmail.com>
2022-12-01 12:24:36 +0100

    Create Row component and display rows of movies fetched from the API

---
+++ src/Row.js
@@ -0,0 +1,42 @@
+import axios from './axios';
+import React, { useEffect, useState } from 'react';
+import './Row.css';
+
+const Row = ({ title, fetchUrl, isLargeRow = false }) => {
+  const [movies, setMovies] = useState([]);
+
+  const baseUrl = 'https://image.tmdb.org/t/p/original';
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const {
+        data: { results }
+      } = await axios.get(fetchUrl);
+      setMovies(results);
+      return results;
+    };
+
+    fetchData();
+  }, [fetchUrl]);
+
+  return (
+    <div className="row">
+      <h1>{title}</h1>
+
+      <div className="row__posters">
+        {movies.map((movie) => (
+          <img
+            className={`row__poster ${isLargeRow && 'row__posterLarge'}`}
+            key={movie.id}
+            src={`${baseUrl}${
+              isLargeRow ? movie.poster_path : movie.backdrop_path
+            }`}
+            alt={movie.name}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Row;
