2fb6a9b2ea205f7071a4030a5e5131057e3f774d
codepanther <pezechigozirim@gmail.com>
2022-12-01 16:41:14 +0100

    Add react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import './App.css';
-import HomeScreen from './HomeScreen';
+import HomeScreen from './screens/HomeScreen';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="app">
-      <HomeScreen />
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<HomeScreen />} />
+        </Routes>
+      </Router>
     </div>
-  )
+  );
 }
 
 export default App;
